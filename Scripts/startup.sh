ace58c7b20ae808425fba1b78d2f8ddbfe6008c9
Brandon Pack <bpack@adobe.com>
2019-04-08 15:35:28 -0600

    Change the Lab to match EMEA summit

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -1,11 +1,11 @@
 #Pull the most recent version of the Lab File
 cd ~/Desktop
-git clone https://github.com/Adobe-Marketing-Cloud-Activation/Summit2019Lab.git SummitLab2019-L779
-cd SummitLab2019-L779
+git clone https://github.com/Adobe-Marketing-Cloud-Activation/Summit2019Lab.git SummitLab2019-TL07
+cd SummitLab2019-TL07
 git fetch --all
 git pull 
 
 # Launch the localhost pages
 open -a Google\ Chrome "http://launch-demo.adobe.com/"
-open -a Google\ Chrome "https://aem.enablementadobe.com/content/we-retail/us/en.html"
+open -a Google\ Chrome "http://localhost"
 open -a Google\ Chrome "http://bit.ly/Lab779"
