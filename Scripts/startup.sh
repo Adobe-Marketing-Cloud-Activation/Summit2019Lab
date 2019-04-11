b035d4786652029a5ac8be85ced270d0a42629f7
Brandon Pack <bpack@adobe.com>
2019-04-10 18:13:07 -0600

    Added the MAMP configuration changes

--- Scripts/startup.sh
+++ Scripts/startup.sh
@@ -1,11 +1,38 @@
-#Pull the most recent version of the Lab File
+#!/bin/sh
+
+# Remove the directory if already there to get a fresh copy
+if [ -d ~/Desktop/SummitLab2019-TL07 ]
+then
+    echo "Folder Already Exists"
+    rm -Rf ~/Desktop/SummitLab2019-TL07
+fi
+
+# Pull the most recent version of the Lab File
+echo "Download Lab Assets"
 cd ~/Desktop
-git clone https://github.com/Adobe-Marketing-Cloud-Activation/Summit2019Lab.git SummitLab2019-TL07
+git clone --recurse-submodules https://github.com/Adobe-Marketing-Cloud-Activation/Summit2019Lab.git SummitLab2019-TL07
 cd SummitLab2019-TL07
 git fetch --all
 git pull 
+cd Sites/WeRetail
+git submodule init
+git submodule update
+
+# Make sure MAMP is pointed at the right location
+echo "Ensure MAMP Configuration"
+/Applications/MAMP/conf/apache/httpd.conf
+sed -i "" '190,200s_DocumentRoot\ .*_DocumentRoot "/Users/tl07/Desktop/SummitLab2019-TL07/Sites/WeRetail"_' /Applications/MAMP/conf/apache/httpd.conf
+sed -i "" '210,225s_<Directory\ ".*">_<Directory "/Users/tl07/Desktop/SummitLab2019-TL07/Sites/WeRetail">_' /Applications/MAMP/conf/apache/httpd.conf
+
+# Restart MAMP
+echo "Restart MAMP After Config"
+open /Applications/MAMP/MAMP.app/
+cd /Applications/MAMP/bin
+./stop.sh
+./start.sh
 
 # Launch the localhost pages
+echo "Open Chrome Tabs"
 open -a Google\ Chrome "http://launch-demo.adobe.com/"
 open -a Google\ Chrome "http://localhost/content/we-retail/us/en.html"
 open -a Google\ Chrome "http://bit.ly/Lab779"
